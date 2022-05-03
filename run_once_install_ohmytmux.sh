8c6acd6094eae4af8cad513f749134dd13bd68c0
Jack Deng <673298622@qq.com>
2022-05-03 13:15:55 +0800

    update

--- run_once_install_ohmytmux.sh
+++ run_once_install_ohmytmux.sh
@@ -2,10 +2,10 @@
 tmuxconfPath=$HOME/.tmux.conf
 tmuxlocaPath=$HOME/.tmux.conf.local
 
-if [[! -f "$tmuxconfPath"]]; then
+if [[ ! -f "$tmuxconfPath" ]]; then
   ln -s -f $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
 fi
 
-if [[! -f "$tmuxlocaPath"]]; then
+if [[ ! -f "$tmuxlocaPath" ]]; then
   cp $HOME/.tmux/.tmux.conf.local $HOME/.tmux.conf.local
 fi
