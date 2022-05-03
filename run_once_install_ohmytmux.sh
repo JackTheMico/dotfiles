cd412d126e5fe26ca6ec112e5a6b234cda7f6a41
Jack Deng <673298622@qq.com>
2022-05-03 13:11:48 +0800

    add install script for ohmytmux

---
+++ run_once_install_ohmytmux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+tmuxconfPath = $HOME/.tmux.conf
+tmuxlocaPath = $HOME/.tmux.conf.local
+
+if [! -f "$tmuxconfPath"]; then
+  ln -s -f $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
+fi
+
+if [ ! -f "$tmuxlocaPath"]; then
+  cp $HOME/.tmux/.tmux.conf.local $HOME/.tmux.conf.local
+fi
