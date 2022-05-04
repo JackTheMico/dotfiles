3eb948832d8b6731f88aedcdc87eed93514e43dd
Jack Deng <673298622@qq.com>
2022-05-04 11:20:04 +0800

    update install.sh
    Signed-off-by: Jack Deng <673298622@qq.com>

--- codespaces/install.sh
+++ codespaces/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/install.sh" -O font_install.sh
-chmod +x ./font_install.sh
-sh ./font_install.sh
+chmod +x font_install.sh
+./font_install.sh
