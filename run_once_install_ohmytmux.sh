a5391809b28e43ed3edcace641ddea6dd8559e0f
Jack Deng <673298622@qq.com>
2022-05-04 21:23:44 +0800

    Update rofi-translate install
    Signed-off-by: Jack Deng <673298622@qq.com>

--- run_once_install_ohmytmux.sh
+++ run_once_install_ohmytmux.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 tmuxconfPath=$HOME/.tmux.conf
 tmuxlocaPath=$HOME/.tmux.conf.local
+translateGitPath=$HOME/.rofi-translate
+rofiTransPath=$HOME/.local/bin/rofi_trans
+rofiTransBriefPath=$HOME/.local/bin/rofi_trans_brief
+rofiTransDeletePath=$HOME/.local/bin/rofi_trans_delete
+rofiTransVerbosePath=$HOME/.local/bin/rofi_trans_verbose
+rofiVerbosePath=$HOME/.local/bin/rofi_verbose
 
 if [[ ! -f "$tmuxconfPath" ]]; then
   ln -s -f $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
@@ -8,4 +14,24 @@ fi
 
 if [[ ! -f "$tmuxlocaPath" ]]; then
   cp $HOME/.tmux/.tmux.conf.local $HOME/.tmux.conf.local
+fi
+
+if [[ ! -f "$rofiTransPath" ]]; then
+  ln -s -f $HOME/.rofi-translate/rofi_trans $HOME/.local/bin/rofi_trans
+fi
+
+if [[ ! -f "$rofiTransBriefPath" ]]; then
+  ln -s -f $HOME/.rofi-translate/rofi_trans_brief $HOME/.local/bin/rofi_trans_brief
+fi
+
+if [[ ! -f "$rofiTransDeletePath" ]]; then
+  ln -s -f $HOME/.rofi-translate/rofi_trans_delete $HOME/.local/bin/rofi_trans_delete
+fi
+
+if [[ ! -f "$rofiTransVerbosePath" ]]; then
+  ln -s -f $HOME/.rofi-translate/rofi_trans_verbose $HOME/.local/bin/rofi_trans_verbose
+fi
+
+if [[ ! -f "$rofiVerbosePath" ]]; then
+  ln -s -f $HOME/.rofi-translate/rofi_verbose $HOME/.local/bin/rofi_verbose
 fi
