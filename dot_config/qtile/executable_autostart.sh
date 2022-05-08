e62aeab081b53734166866d8298c081395550bed
Jack Deng <673298622@qq.com>
2022-05-08 21:21:42 +0800

    Update dot_config/qtile/executable_autostart.sh

--- dot_config/qtile/executable_autostart.sh
+++ dot_config/qtile/executable_autostart.sh
@@ -4,6 +4,7 @@ systemctl --user start greenclip.service
 systemctl --user start himalaya-watcher
 systemctl --user start redshift.service
 systemctl --user start dunst.service
+systemctl --user start syncthing.service
 touch /tmp/himalaya-counter
 picom -bc --active-opacity 0.9
 nutstore &
