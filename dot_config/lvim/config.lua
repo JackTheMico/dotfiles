d75db0de1621842a2093eedfa1946461f81a87d5
Jack Deng <673298622@qq.com>
2022-05-09 15:30:04 +0800

    Update dot_config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -576,64 +576,22 @@ orgmode.setup({
 })
 
 --luasnip
-local ls        = require("luasnip")
-local s         = ls.snippet
-local sn        = ls.snippet_node
-local t         = ls.text_node
-local i         = ls.insert_node
-local c         = ls.choice_node
-local d         = ls.dynamic_node
-local r         = ls.restore_node
-local fmt       = require("luasnip.extras.fmt").fmt
 
--- see latex infinite list for the idea. Allows to keep adding arguments via choice nodes.
-local function py_init()
-  return
-    c(1, {
-      t(""),
-      sn(1, {
-        t(", "),
-        i(1),
-        d(2, py_init)
-      })
-    })
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local sn = ls.snippet_node
+-- local fmt = require("luasnip.extras.fmt").fmt
+local d = ls.dynamic_node
+local date_input = function(args, snip, old_state, fmt)
+	local fmt = fmt or "%Y-%m-%d"
+	return sn(nil, i(1, os.date(fmt)))
 end
-
--- splits the string of the comma separated argument list into the arguments
--- and returns the text-/insert- or restore-nodes
-local function to_init_assign(args)
-  local tab = {}
-  local a = args[1][1]
-  if #(a) == 0 then
-    table.insert(tab, t({"", "\tpass"}))
-  else
-    local cnt = 1
-    for e in string.gmatch(a, " ?([^,]*) ?") do
-      if #e > 0 then
-        table.insert(tab, t({"","\tself."}))
-        -- use a restore-node to be able to keep the possibly changed attribute name
-        -- (otherwise this function would always restore the default, even if the user
-        -- changed the name)
-        table.insert(tab, r(cnt, tostring(cnt), i(nil,e)))
-        table.insert(tab, t(" = "))
-        table.insert(tab, t(e))
-        cnt = cnt+1
-      end
-    end
-  end
-  return
-    sn(nil, tab)
-end
-
--- create the actual snippet
-s("pyinit", fmt(
-  [[def __init__(self{}):{}]],
-  {
-    d(1, py_init),
-    d(2, to_init_assign, {1})
-  }))
-
-
+ls.add_snippets("all", {
+	s("time", {
+		d(1, date_input, {}, { user_args = { "%A, %B %d of %Y" } }),
+	}),
+})
 require("luasnip.loaders.from_vscode").lazy_load({ path = {"~/.config/lvim/snippets" } })
 
 -- dapui
