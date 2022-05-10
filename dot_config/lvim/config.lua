9f0039e44221bf49e4d50736ea71d3d4732d40a6
Jack Deng <673298622@qq.com>
2022-05-10 09:58:31 +0800

    Update dot_config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -537,24 +537,24 @@ lvim.plugins = {
 }
 
 -- org setup
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+-- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 -- Load custom tree-sitter grammar for org filetype
 local orgmode = require("orgmode")
 orgmode.setup_ts_grammar()
-parser_config.org = {
-  install_info = {
-    url = 'https://github.com/milisims/tree-sitter-org',
-    revision = 'f110024d539e676f25b72b7c80b0fd43c34264ef',
-    files = { 'src/parser.c', 'src/scanner.cc' },
-  },
-  filetype = 'org',
-}
+-- parser_config.org = {
+--   install_info = {
+--     url = 'https://github.com/milisims/tree-sitter-org',
+--     revision = 'f110024d539e676f25b72b7c80b0fd43c34264ef',
+--     files = { 'src/parser.c', 'src/scanner.cc' },
+--   },
+--   filetype = 'org',
+-- }
 
 require 'nvim-treesitter.configs'.setup {
   -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
   highlight = {
     enable = true,
-    disable = { 'org' }, -- Remove this to use TS highlighter for some of the highlights (Experimental)
+    disable = { 'org' },
     additional_vim_regex_highlighting = { 'org' }, -- Required since TS highlighter doesn't support all syntax features (conceal)
   },
   ensure_installed = { 'org' }, -- Or run :TSUpdate org
