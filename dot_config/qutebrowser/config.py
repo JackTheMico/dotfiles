125cd5d24fae39b67a583e14b6ba7e9c0a56f797
Jack Deng <673298622@qq.com>
2022-05-27 16:04:00 +0800

    Update dot_config/qutebrowser/config.py

--- dot_config/qutebrowser/config.py
+++ dot_config/qutebrowser/config.py
@@ -292,8 +292,8 @@ c.fonts.default_size = '13pt'
 
 # Bindings for normal mode
 config.bind(',M', 'spawn mpv {url}')
-config.bind(',l', 'spawn --userscript qute-lastpass')
-config.bind(',m', 'hint links spawn mpv {hint-url}')
+config.bind(',l', 'spawn --userscript qute-keypass')
+config.bind(',m', 'hint links spawn mpv {hintyu-url}')
 config.bind(',o', 'hint links spawn mpv --http-proxy=http://127.0.0.1:7890 {hint-url}')
 config.bind(',p', 'config-cycle content.proxy http://localhost:8080 system --print')
 config.bind(',s', 'config-cycle content.proxy http://localhost:7890 system --print')
