8fc200e6a96070e68e8332a84671cd94115dc02f
Jack Deng <673298622@qq.com>
2022-06-02 17:28:26 +0800

    Update run_gh_extension_install.sh
    Signed-off-by: Jack Deng <673298622@qq.com>

--- run_gh_extension_install.sh
+++ run_gh_extension_install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-check_extensions=`gh extension list`
 echo "gh extension list result: $check_extensions"
-nostr="no installed extensions found"
-
-if [[ "$check_extensions" == "$nostr" ]]; then
+gh extension upgrade mislav/gh-branch
+check_extensions=$?
+if [[ $check_extensions == 1 ]]; then
   echo "No extensions found. Installing..."
   gh extension install mislav/gh-branch
   gh extension install gennaro-tedesco/gh-s
   gh extension install k1LoW/gh-grep
-  gh extension install markdown-preview
+  gh extension install yusukebe/gh-markdown-preview
   gh extension install gennaro-tedesco/gh-f
   gh extension install meiji163/gh-notify
   gh extension install rnorth/gh-combine-prs
@@ -19,9 +18,11 @@ else
   gh extension upgrade mislav/gh-branch
   gh extension upgrade gennaro-tedesco/gh-s
   gh extension upgrade k1LoW/gh-grep
-  gh extension upgrade markdown-preview
+  gh extension upgrade yusukebe/gh-markdown-preview
   gh extension upgrade gennaro-tedesco/gh-f
   gh extension upgrade meiji163/gh-notify
   gh extension upgrade rnorth/gh-combine-prs
   gh extension upgrade dlvhdr/gh-dash
+  echo $?
   echo "Extensions upgraded."
+fi
