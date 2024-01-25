82b1e49a034f39dda0e4f8f9bbe047f7373308a7
jackwenyoung <dlwxxxdlw@gmail.com>
2024-01-25 15:41:20 +0800

    Update .config/lvim/config.lua

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -8,6 +8,17 @@ lvim.format_on_save = true
 
 lvim.plugins = {
   {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    module = "persistence",
+    config = function()
+      require("persistence").setup {
+        dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
+        options = { "buffers", "curdir", "tabpages", "winsize" },
+      }
+    end,
+  },
+  {
     'Exafunction/codeium.vim',
     event = 'BufEnter'
   },
