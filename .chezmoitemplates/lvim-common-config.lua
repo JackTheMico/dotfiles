0d286486b571d6243950aaed59918a4c67e156dc
jackwenyoung <dlwxxxdlw@gmail.com>
2024-02-15 10:56:40 +0800

    update lvim config

--- .chezmoitemplates/lvim-common-config.lua
+++ .chezmoitemplates/lvim-common-config.lua
@@ -26,7 +26,7 @@ lvim.plugins = {
   {
     "folke/persistence.nvim",
     event = "BufReadPre", -- this will only start session saving when an actual file was opened
-    module = "persistence",
+    lazy = true,
     config = function()
       require("persistence").setup {
         dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
