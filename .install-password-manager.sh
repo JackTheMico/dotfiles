175473d4494e4586e11e14bdaa309e12d32f2223
Jack Wenyoung <jackwenyoung@DESKTOP-VG15AEV.>
2024-09-22 16:04:52 +0800

    adjust keepassxc install script

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -3,18 +3,22 @@
 # exit immediately if password-manager-binary is already in $PATH
 type keepassxc-cli >/dev/null 2>&1 && exit
 
-case "$(uname -s)" in
+case "$(cat /etc/issue | cut -d ' ' -f1)" in
 Darwin)
-    # commands to install password-manager-binary on Darwin
-    ;;
-Linux)
-    # commands to install password-manager-binary on Linux
-    # use pacman because I use Manjaro
-    sudo pacman-mirrors --country China
-    sudo pacman -S --noconfirm --needed keepassxc
-    ;;
+  # commands to install password-manager-binary on Darwin
+  ;;
+Manjaro)
+  # commands to install password-manager-binary on Linux
+  # use pacman because I use Manjaro
+  sudo pacman-mirrors --country China
+  sudo pacman -S --noconfirm --needed keepassxc
+  ;;
+Kali)
+  sudo apt update
+  sudo apt install -y keepassxc
+  ;;
 *)
-    echo "unsupported OS"
-    exit 1
-    ;;
+  echo "unsupported OS"
+  exit 1
+  ;;
 esac
