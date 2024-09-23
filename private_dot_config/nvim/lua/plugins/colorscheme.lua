6057291a1346f12e1c68ad231f2dfaea9fe5aac6
jackwenyoung <dlwxxxdlw@gmail.com>
2024-09-23 16:40:23 +0800

    Update .config/nvim/lua/plugins/colorscheme.lua

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
-  { "folke/tokyonight.nvim" },
+  { "Mofiqul/dracula.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "LazyVim/LazyVim", opts = {
-    colorscheme = "tokyonight",
+    colorscheme = "catppuccin-frappe",
   } },
 }
