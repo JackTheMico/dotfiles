4044dbe92061b3cecc7c43a9d91ba29cf7605027
jackwenyoung <dlwxxxdlw@gmail.com>
2024-11-22 09:48:59 +0800

    update lazyvim confirm

--- private_dot_config/nvim/lua/plugins/confirm.lua
+++ private_dot_config/nvim/lua/plugins/confirm.lua
@@ -2,7 +2,16 @@ return {
 	"stevearc/conform.nvim",
 	opts = {
 		formatters_by_ft = {
+			lua = { "stylua" },
+			yaml = { "yamlfmt" },
 			python = { "isort", "ruff_format" },
+			markdown = { "markdownlint-cli2" },
+			json = { "biome" },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
 		},
 	},
 }
