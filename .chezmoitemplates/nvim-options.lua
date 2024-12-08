4b9723752ec2d5a0fcf7c9ded66e5284c24f4e4a
jackwenyoung <dlwxxxdlw@gmail.com>
2024-12-08 17:13:29 +0800

    update conform format

--- .chezmoitemplates/nvim-options.lua
+++ .chezmoitemplates/nvim-options.lua
@@ -8,3 +8,7 @@ vim.g.lazyvim_python_lsp = "pylsp"
 vim.g.lazyvim_python_ruff = "ruff"
 -- LSP Server to use for PHP.
 vim.g.lazyvim_php_lsp = "intelephense"
+
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = true
