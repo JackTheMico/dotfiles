30891a130f13be8539e58b631615430eae7e9ad4
Jack Wenyoung <dlwxxxdlw@gmail.com>
2025-08-19 12:13:39 +0800

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,10 +6,14 @@ set -gx GPG_TTY $(tty)
 set -gx http_proxy 'http://127.0.0.1:7897'
 set -gx https_proxy 'http://127.0.0.1:7897'
 set -gx PATH $HOME/.local/bin $PATH
+set -gx PNPM_HOME $HOME/pnpm_home
 
 zoxide init fish | source
 starship init fish | source
 alias czi chezmoi
+alias ls eza
+alias la eza -a
+alias lt eza -T
 alias md mkdir
 alias lg lazygit
 alias ff fastfetch
