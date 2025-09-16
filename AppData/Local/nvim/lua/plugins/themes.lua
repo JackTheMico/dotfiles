80e007aa1764c9f90abcdd0cf4da48c3c968b3a0
Jack Wenyoung <dlwxxxdlw@gmail.com>
2025-09-16 12:09:13 +0800

    Add AppData/Local/nvim/lua/plugins/themes.lua

---
+++ AppData/Local/nvim/lua/plugins/themes.lua
@@ -0,0 +1,37 @@
+return {
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+  { "rebelot/kanagawa.nvim" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require("everforest").setup({
+        -- Your config here
+      })
+    end,
+  },
+  {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    -- config = function()
+    --     vim.g.zenbones_darken_comments = 45
+    --     vim.cmd.colorscheme('zenbones')
+    -- end
+  },
+}
