8825357a5110adf8d7020fef18bb4eadf43fd83f
Jack Wenyoung <dlwxxxdlw@gmail.com>
2026-07-15 15:44:30 +0800

    Update AppData/Roaming/yazi/config/init.lua Add AppData/Roaming/yazi/config/package.toml

--- AppData/Roaming/yazi/config/init.lua
+++ AppData/Roaming/yazi/config/init.lua
@@ -1,26 +1,19 @@
-require("full-border"):setup({
-	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.ROUNDED,
+require("full-border"):setup()
+
+require("starship"):setup()
+
+require("easyjump"):setup()
+
+require("augment-command"):setup({
+    smooth_scrolling = true,
 })
-require("flash"):setup({
-	unmatched_style = { fg = "darkgrey" },
-	match_style = { fg = "black", bg = "blue" },
-	label_style = { fg = "black", bg = "magenta", bold = true },
-})
-require("starship"):setup({
-	-- Hide flags (such as filter, find and search). This can be beneficial for starship themes
-	-- which are intended to go across the entire width of the terminal.
-	hide_flags = false,
-	-- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
-	flags_after_prompt = true,
-	-- Custom starship configuration file to use
-	-- config_file = "~/.config/starship.toml", -- Default: nil
-	-- Whether to enable support for starship's right prompt (i.e. `starship prompt --right`).
-	show_right_prompt = true,
-	-- Whether to hide the count widget, in case you want only your right prompt to show up. Only has
-	-- an effect when `show_right_prompt = true`
-	hide_count = true,
-	-- Separator to place between the right prompt and the count widget. Use `count_separator = ""`
-	-- to have no space between the widgets.
-	count_separator = " ",
-})
+
+local bookmarks = {
+  { tag = "Desktop",   path = "~/Desktop",   key = "d" },
+  { tag = "Documents", path = "~/Documents", key = "D" },
+  { tag = "Downloads", path = "~/Downloads", key = "o" },
+}
+require("whoosh"):setup {
+  bookmarks = bookmarks,
+  jump_notify = false,
+}
