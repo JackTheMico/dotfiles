d8d4cceceaaf6323743349d09f0e85ce6b04055e
Jack Wenyoung <dlwxxxdlw@gmail.com>
2026-07-30 08:23:52 +0800

    Update .config/fish/functions/fzfchezmoi.fish

--- dot_config/fish/functions/fzfchezmoi.fish
+++ dot_config/fish/functions/fzfchezmoi.fish
@@ -3,7 +3,7 @@ function ce --description 'Fuzzy search chezmoi managed files and edit'
     # 使用 chezmoi managed 获取相对路径，fzf 预览实际文件内容
     set -l target (chezmoi managed 2>/dev/null | \
     fzf --height 40% --layout=reverse \
-        --preview 'bat --color=always "$HOME/{}" 2>/dev/null || cat "$HOME/{}"' \
+        --preview 'bat --color=always "$HOME/{}" 2>/dev/null' \
         --preview-window=right:60%:wrap)
 
     if test -n "$target"
